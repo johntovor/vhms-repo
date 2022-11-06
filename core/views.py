8317dbd674d07b39da431aa16bfd3afc5ea879c0
John <ideanitypro@gmail.com>
2022-11-06 16:01:57 +0000

    first commit

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from re import template
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    template_name = 'core/index.html'
+    context = {}
+    return render(request, template_name, context)
